73c44d23cab5356e14083896ef99ffadbe1c83c3
kat2n <katsumata.coffee.150yen@gmail.com>
2018-10-10 15:07:54 -0700

    fix ShowLogicErrors

--- src/ShowLogicErrors.java
+++ src/ShowLogicErrors.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class ShowLogicErrors {
     public static void main(String[] args) {
-        System.out.println("Celsius 35 is Fahrenheit degree ");
-        System.out.println((9.0 / 5) * 35 + 32);
+        double fahrenheit, celsius;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Tell me the Fahrenheit value to convert to Celsius");
+
+        fahrenheit = input.nextDouble();
+
+        celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println("Fahrenheit " + fahrenheit + " is Celsius " + celsius);
+
+        input.close();
     }
 }
